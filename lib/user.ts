7378f9e31f197c2ed9f393ca384673658b9d35a1
uhyo <uhyo@uhy.ooo>
2018-06-19 23:56:16 +0900

    do not user null as placeholder ObjectID

--- lib/user.ts
+++ lib/user.ts
@@ -1,4 +1,4 @@
-import randomstring=require('random-string');
+import randomstring = require('random-string');
 import * as myuser from 'my-user';
 
 import {
@@ -14,15 +14,15 @@ export import User = myuser.User;
 export import UserConfig = myuser.UserConfig;
 export import UserConfigOptions = myuser.UserConfigOptions;
 
-export type UserCallback<T> = (err:any, user:User<T> | null)=>void;
-export type UsersCallback<T> = (err:any, users:Array<User<T>> | null)=>void;
+export type UserCallback<T> = (err: any, user: User<T> | null) => void;
+export type UsersCallback<T> = (err: any, users: Array<User<T>> | null) => void;
 
-export interface UserDoc<T extends object>{
-    _id:ObjectID;
-    id:string | null;
-    version:number;
-    salt:string;
-    password:string;
+export interface UserDoc<T extends object> {
+    _id: ObjectID;
+    id: string | null;
+    version: number;
+    salt: string;
+    password: string;
     data: T;
 }
 
@@ -31,16 +31,16 @@ export type UserData<T> = T & {
 };
 
 
-export function init(options?: Partial<UserConfigOptions>):UserConfig{
+export function init(options?: Partial<UserConfigOptions>): UserConfig {
     //TODO
     return myuser.init(options);
 }
 
 //UserDoc -> User
-export function load<T extends object>(userconfig:UserConfig, doc: null): null;
-export function load<T extends object>(userconfig:UserConfig, doc:UserDoc<T>): User<UserData<T>>;
-export function load<T extends object>(userconfig:UserConfig, doc:UserDoc<T> | null): User<UserData<T>> | null{
-    if(doc==null){
+export function load<T extends object>(userconfig: UserConfig, doc: null): null;
+export function load<T extends object>(userconfig: UserConfig, doc: UserDoc<T>): User<UserData<T>>;
+export function load<T extends object>(userconfig: UserConfig, doc: UserDoc<T> | null): User<UserData<T>> | null {
+    if (doc == null) {
         return null;
     }
     const result = userconfig.create<UserData<T>>();
@@ -60,116 +60,116 @@ export function load<T extends object>(userconfig:UserConfig, doc:UserDoc<T> | n
 
 //User -> UserDoc
 export function toDoc<T extends Object>(user: null): null;
-export function toDoc<T extends object>(user:User<UserData<T>>): UserDoc<T>;
-export function toDoc<T extends object>(user:User<UserData<T>> | null): UserDoc<T> | null{
-    if(user==null){
+export function toDoc<T extends object>(user: User<UserData<T>>): UserDoc<T>;
+export function toDoc<T extends object>(user: User<UserData<T>> | null): UserDoc<T> | null {
+    if (user == null) {
         return null;
     }
     //TODO
     const data = user.getData();
     const result: UserDoc<T> = {
         _id: data._id,
-        id:user.id,
-        version:user.version,
-        salt:user.salt,
-        password:user.password,
+        id: user.id,
+        version: user.version,
+        salt: user.salt,
+        password: user.password,
         data,
     };
     return result;
 }
 
-export interface ManagerOptions{
+export interface ManagerOptions {
     //user id length
-    userIdLength?:number;
+    userIdLength?: number;
 }
 
 //User manager
 export class Manager<T extends object>{
-    constructor(private userconfig:UserConfig, private db: Db, private collection:string, private options:ManagerOptions){
+    constructor(private userconfig: UserConfig, private db: Db, private collection: string, private options: ManagerOptions) {
     }
     //
     //low level user manipulation
-    findOneUser(query:any,callback:UserCallback<UserData<T>>):void{
-        this.db.collection(this.collection,(err,coll)=>{
-            if(err){
+    findOneUser(query: any, callback: UserCallback<UserData<T>>): void {
+        this.db.collection(this.collection, (err, coll) => {
+            if (err) {
                 callback(err, null);
                 return;
             }
-            coll.findOne(query,(err, doc:UserDoc<T>)=>{
-                if(err){
-                    callback(err,null);
+            coll.findOne(query, (err, doc: UserDoc<T>) => {
+                if (err) {
+                    callback(err, null);
                     return;
                 }
-                callback(null,load(this.userconfig,doc));
+                callback(null, load(this.userconfig, doc));
             });
         });
     }
-    findUsers(query:any, callback:UsersCallback<UserData<T>>): void;
-    findUsers(query:any, options:any, callback:UsersCallback<UserData<T>>): void;
-    findUsers(query:any, arg2:any, arg3?:any): void{
-        let options:any, callback:UsersCallback<UserData<T>>;
-        if("function"===typeof arg2){
-            options={}, callback=arg2;
-        }else{
-            options=arg2, callback=arg3;
+    findUsers(query: any, callback: UsersCallback<UserData<T>>): void;
+    findUsers(query: any, options: any, callback: UsersCallback<UserData<T>>): void;
+    findUsers(query: any, arg2: any, arg3?: any): void {
+        let options: any, callback: UsersCallback<UserData<T>>;
+        if ("function" === typeof arg2) {
+            options = {}, callback = arg2;
+        } else {
+            options = arg2, callback = arg3;
         }
 
-        this.db.collection(this.collection, (err,coll)=>{
-            if(err){
-                callback(err,null);
+        this.db.collection(this.collection, (err, coll) => {
+            if (err) {
+                callback(err, null);
                 return;
             }
-            coll.find(query,options).toArray((err,docs:Array<UserDoc<T>>)=>{
-                if(err){
-                    callback(err,null);
+            coll.find(query, options).toArray((err, docs: Array<UserDoc<T>>) => {
+                if (err) {
+                    callback(err, null);
                     return;
                 }
-                callback(null, docs.map((doc)=>{
+                callback(null, docs.map((doc) => {
                     return load(this.userconfig, doc) as User<UserData<T>>;
-                }).filter(u=> u != null));
+                }).filter(u => u != null));
             });
         });
     }
-    createNewUser(data: T, callback:UserCallback<UserData<T>>):void{
+    createNewUser(data: T, callback: UserCallback<UserData<T>>): void {
         //generate random user id
-        var t=this;
-        generate((id:string)=>{
+        var t = this;
+        generate((id: string) => {
             //new id is found
             const u = this.userconfig.create<UserData<T>>(id);
             u.setData({
                 ... (data as any),
-                _id: null as any,
+                _id: undefined as any,
             } as UserData<T>);
             //save
-            this.db.collection(this.collection,(err,coll)=>{
-                if(err){
-                    callback(err,null);
+            this.db.collection(this.collection, (err, coll) => {
+                if (err) {
+                    callback(err, null);
                     return;
                 }
-                coll.insertOne(toDoc(u),(err,result)=>{
-                    if(err){
-                        callback(err,null);
+                coll.insertOne(toDoc(u), (err, result) => {
+                    if (err) {
+                        callback(err, null);
                         return;
                     }
 
                     u.writeData({
-                        _id:result.insertedId,
+                        _id: result.insertedId,
                     } as any);
-                    callback(null,u);
+                    callback(null, u);
                 });
             });
         });
-        function generate(callback2:(id:string)=>void):void{
-            const id = randomstring({length: t.options.userIdLength});
+        function generate(callback2: (id: string) => void): void {
+            const id = randomstring({ length: t.options.userIdLength });
             t.findOneUser({
-                id:id
-            }, (err,user: User<UserData<T>>)=>{
-                if(err){
-                    callback(null,null);
+                id: id
+            }, (err, user: User<UserData<T>>) => {
+                if (err) {
+                    callback(null, null);
                     return;
                 }
 
-                if(user!=null){
+                if (user != null) {
                     //this id already exists
                     generate(callback2);
                     return;
@@ -178,36 +178,36 @@ export class Manager<T extends object>{
             });
         }
     }
-    saveUser(u: User<UserData<T>>,callback:Callback<any>):void{
-        this.db.collection(this.collection,(err,coll)=>{
-            if(err){
-                callback(err,null);
+    saveUser(u: User<UserData<T>>, callback: Callback<any>): void {
+        this.db.collection(this.collection, (err, coll) => {
+            if (err) {
+                callback(err, null);
                 return;
             }
-            coll.updateOne({id:u.id},toDoc(u),callback);
+            coll.updateOne({ id: u.id }, toDoc(u), callback);
         });
     }
-    deleteUser(u: User<UserData<T>>, callback:Callback<any>):void{
-        this.db.collection(this.collection,(err,coll)=>{
-            if(err){
-                callback(err,null);
+    deleteUser(u: User<UserData<T>>, callback: Callback<any>): void {
+        this.db.collection(this.collection, (err, coll) => {
+            if (err) {
+                callback(err, null);
                 return;
             }
-            coll.deleteOne({id:u.id},callback);
+            coll.deleteOne({ id: u.id }, callback);
         });
     }
 
     //are
-    existsUser(userid:string, callback:Callback<boolean>):void{
-        this.db.collection(this.collection,(err,coll)=>{
-            if(err){
-                callback(err,null);
+    existsUser(userid: string, callback: Callback<boolean>): void {
+        this.db.collection(this.collection, (err, coll) => {
+            if (err) {
+                callback(err, null);
                 return;
             }
-            coll.findOne({id:userid},{
-                fields: {id:1},
-            }, (err,doc)=>{
-                callback(err, doc!=null);
+            coll.findOne({ id: userid }, {
+                fields: { id: 1 },
+            }, (err, doc) => {
+                callback(err, doc != null);
             });
         });
     }
